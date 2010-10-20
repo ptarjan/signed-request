c4476c3c04dd84cebb5e7c2f59db6cce306ac95c
Naitik Shah <n@daaku.org>
2010-10-20 18:32:52 +0000

    handle java deps

--- sample.java.sh
+++ sample.java.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-javac sample.java
-java sample
+if [ ! -d .jars ]; then
+  mkdir .jars
+  cd .jars
+  curl http://www.reverse.net/pub/apache//commons/codec/binaries/commons-codec-1.4-bin.tar.gz | tar xz --strip-components=1 commons-codec-1.4/commons-codec-1.4.jar
+  curl -O http://json-simple.googlecode.com/files/json_simple-1.1.jar
+  cd ..
+fi
+
+CP=$PWD/.jars/commons-codec-1.4.jar:$PWD/.jars/json_simple-1.1.jar:$PWD
+javac -cp $CP sample.java
+java -cp $CP sample
